4e61dae95af12aefe1331fa083e4363bb64b9d33
Aarav <lodhaaarav12005@gmail.com>
2024-02-12 21:07:57 +0530

    added routing

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var expressSession = require("express-session");
-const passport=require("passport");
+const passport = require("passport");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -18,7 +18,7 @@ app.use(expressSession({
   resave:false,
   saveUninitialized: false,
   secret:"nothing",
-}))
+}))  
 app.use(passport.initialize());
 app.use(passport.session());
 passport.serializeUser(usersRouter.serializeUser()); 
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,17 @@ var express = require("express");
 var router = express.Router();
 const userModel = require("./users");
 const postModel = require("./posts");
+const passport=require("passport");
+const localStrategy = require("passport-local");
+passport.authenticate(new localStrategy(userModel.authenticate()));
 
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
-});
+})
+
+router.get("/profile" , isLoggedIn, function(req,res,next){
+  res.send("profile");
+})
 
 router.post("/register", function(req,res){
   const userdata = new userModel({
@@ -13,6 +20,40 @@ router.post("/register", function(req,res){
     email: req.body.email,
     fullName: req.body.fullName,
   })
+
+  userModel.register(userdata, req.body.password)
+  .then(function(){
+    passport.authenticate("local")(req,res,function(){
+      res.redirect("/profile");
+    })
+  })
 })
+
+router.post("/login", passport.authenticate("local",{
+  successRedirect: "/profile",
+  failureRedirect: "/"
+}) ,function(req,res){   
+})
+
+router.post("/logout", function(req,res,next){ 
+  req.logout(function(err){
+    if(err)
+    {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+})
+
+function isLoggedIn(req,res,next){
+  if(req.isAuthenticated())
+  {
+    return next();
+  }
+  else
+  {
+    res.redirect("/");
+  }
+}
 
 module.exports = router;
